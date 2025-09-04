1cb9cea9cfef483db227455c9baea066871aaa18
Victor Medrado <victormedrado49@gmail.com>
2025-09-04 14:35:49 -0300

    styles(screens/Home): improves Home screen to be responsive

--- src/screens/Home/Home.styles.ts
+++ src/screens/Home/Home.styles.ts
@@ -1,14 +1,68 @@
 import styled from "styled-components";
 
+export const MainContainer = styled.div`
+  height: 100vh;
+  padding-top: 5rem;
+`;
+
 export const Container = styled.div`
   padding: 2rem;
   display: flex;
   flex-direction: column;
   gap: 2rem;
+  max-width: 56rem;
+  margin: 0 auto;
+
+  @media (max-width: 768px) {
+    padding: 1rem;
+    gap: 1.25rem;
+    max-width: 100%;
+  }
+`;
+
+export const Header = styled.div`
+  text-align: center;
+`;
+
+export const HeaderAppName = styled.div`
+  display: inline-flex;
+  align-items: center;
+  gap: 0.5rem;
+  padding: 0.5rem 1rem;
+  background-color: rgba(206, 199, 187, 0.25);
+  border-radius: 9999px;
+  backdrop-filter: blur(4px);
+  border: 1px solid rgba(206, 199, 187, 0.55);
+  margin-bottom: 1rem;
+  text-align: center;
+
+  span {
+    font-size: 0.875rem;
+    font-weight: 500;
+    line-height: 1.25rem;
+  }
+`;
+
+export const HeaderTitle = styled.h1`
+  font-weight: 700;
+  font-size: 1.875rem;
+  line-height: 2.25rem;
+  margin-bottom: 0.5rem;
+  color: ${({ theme }) => theme.colors.secondary};
+
+  @media (max-width: 480px) {
+    font-size: 1.25rem;
+    line-height: 1.5rem;
+  }
 `;
 
 export const Grid = styled.div`
   display: grid;
   grid-template-columns: repeat(2, 1fr);
   gap: 1.5rem;
+
+  @media (max-width: 900px) {
+    grid-template-columns: 1fr;
+    gap: 1rem;
+  }
 `;
--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,27 +1,47 @@
 import { useNavigate } from "react-router";
 import { Card } from "../../components";
-import { Container, Grid } from "./Home.styles";
+import {
+  Container,
+  Grid,
+  Header,
+  HeaderAppName,
+  HeaderTitle,
+  MainContainer,
+} from "./Home.styles";
 import { breathingTechniques } from "../../data/techniques";
+import { FlowerLotusIcon } from "@phosphor-icons/react";
 
 const Home = () => {
   const navigate = useNavigate();
 
   return (
-    <Container>
-      <h1>Exercícios de Respiração</h1>
-      <Grid>
-        {breathingTechniques.map((technique) => (
-          <Card
-            key={technique.id}
-            title={technique.title}
-            description={technique.description}
-            icon={technique.icon}
-            tags={technique.tags}
-            onClick={() => navigate(`/breathing/${technique.id}`)}
-          />
-        ))}
-      </Grid>
-    </Container>
+    <main style={{ position: "relative" }}>
+      <MainContainer>
+        <Container>
+          <Header>
+            <HeaderAppName>
+              <FlowerLotusIcon size={24} />
+              <span>Breathe</span>
+            </HeaderAppName>
+
+            <HeaderTitle>Respiração Guiada</HeaderTitle>
+            <p>Técnicas de respiração para relaxamento, foco e bem-estar</p>
+          </Header>
+          <Grid>
+            {breathingTechniques.map((technique) => (
+              <Card
+                key={technique.id}
+                title={technique.title}
+                description={technique.description}
+                icon={technique.icon}
+                tags={technique.tags}
+                onClick={() => navigate(`/breathing/${technique.id}`)}
+              />
+            ))}
+          </Grid>
+        </Container>
+      </MainContainer>
+    </main>
   );
 };
 
